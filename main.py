f113a91863453956d9492e8e229cb24c78ea5333
trkhoi <39881166+trkhoi@users.noreply.github.com>
2019-07-29 10:45:37 +0700

    Add some code so that can run folder 15

---
+++ main.py
@@ -0,0 +1,54 @@
+from __future__ import division
+import os
+import json
+import numpy as np
+from preprocess import preProcessPdf
+from processData import extractData
+import pdftotext
+
+if __name__ == '__main__':
+    PDF_TYPE = "15"
+    fileName = list(filter(lambda pdf: pdf[-3:] == 'pdf' ,os.listdir('../' + PDF_TYPE)))
+    # fileName = ["SGNV33817500.pdf"]
+    print(fileName)
+    for file in fileName:
+        print(file)
+        # Reset Current CONFIG
+        with open('../' + PDF_TYPE + '/' + PDF_TYPE + '.json', 'r', encoding='utf8') as json_file:
+            CONFIG = json.load(json_file)
+        CURR_CONFIG = {}
+        
+        if (PDF_TYPE == "15"):
+            with open('../' + PDF_TYPE + '/' + file, "rb") as f:
+                pdf = pdftotext.PDF(f)
+            if (len(pdf) == 2):
+                CONFIG = CONFIG["multi"]
+            elif (len(pdf) == 3):
+                CONFIG = CONFIG["3"]
+            elif (len(pdf) == 1):
+                CONFIG = CONFIG["1"]
+
+        # Sort CONFIG from top to bottom, from left to right
+        configByColumn = dict(sorted(CONFIG.items(), key=lambda kv: kv[1]['column'][0]))
+        CONFIG = dict(sorted(configByColumn.items(), key=lambda kv: kv[1]['row'][0]))
+        # print(CONFIG)
+
+        # Create config for current pdf
+        for key in CONFIG:
+            CURR_CONFIG[key] = {}
+            CURR_CONFIG[key]['row'] = CONFIG[key]['row']
+            CURR_CONFIG[key]['column'] = CONFIG[key]['column']
+
+        # Preproces PDF
+        fullPdf = preProcessPdf('../' + PDF_TYPE + '/' + file)
+        # for line in fullPdf:
+        #     print(line)
+        # Extract data from PDF
+        extractedData = extractData(fullPdf, CONFIG, CURR_CONFIG)
+
+        # Save extracted result to file
+        with open('../' + PDF_TYPE + '/' + file[:-3] + 'txt', 'w', encoding='utf8') as resultFile:
+            for key in extractedData:
+                resultFile.write("------------------------------------\n")
+                resultFile.write("%s:\n%s\n" % (key, extractedData[key]))
+                resultFile.write("------------------------------------\n")
